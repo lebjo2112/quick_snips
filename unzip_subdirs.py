159eea67d91b2ef6d0a69d65e8dbb443752fdea6
lebjo2112 <lebjo2112@protonmail.com>
2020-10-28 17:54:25 -0400

    add scripts

---
+++ unzip_subdirs.py
@@ -0,0 +1,11 @@
+import os, subprocess
+
+dir = str(input("Enter path to dir: ")) + '/'
+for i in os.listdir(dir):
+    os.chdir(dir + str(i))
+    print("Extracting files in: " + str(i))
+    subprocess.call('~/scripts/unzip_dir.sh', shell=True)
+    for f in os.listdir(dir + str(i)):
+        if ".zip" in str(f):
+            os.remove(str(f))
+    os.chdir(dir)
---
+++ zip_subdirs.py
@@ -0,0 +1,10 @@
+import os, subprocess
+
+dir = str(input("Enter path to dir: ")) + '/'
+for i in os.listdir(dir):
+    os.chdir(dir + str(i))
+    print("Compressing files in:")
+    print(os.getcwd())
+    subprocess.call('~/scripts/zip_dir.sh', shell=True)
+    os.chdir(dir)
+    print(os.getcwd())
